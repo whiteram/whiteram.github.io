34166b414d6506b27bf811d32fd1091d14f812ee
whiteram <17860971637@139.com>
2018-08-10 19:47:14 +0800

    lt fix 23

--- littletheatre/main.js
+++ littletheatre/main.js
@@ -1,27 +1,29 @@
 "use strict";
-
+var width,height; 
 var canvas=document.getElementById("myCanvas");
 var ctx = canvas.getContext('2d');
 canvas.width=width;
 canvas.height=height;
-if(screenWidth<screenHeight){
+var lookdic=function(){
+width=window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+height=window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight; 
+			alert(width);
+		alert(height);
+if(width<height){
 	alert("rotate");
 		canvas.width=height;
 		canvas.height=width;
-	var style="";
-      style += "width:" + height + "px;"; 
-      style += "height:" + width + "px;"; 
+		var style= "width:" + height + "px;height:" + width + "px;"; 
 		style+="transform: rotate(90deg);-ms-transform: rotate(90deg);-webkit-transform: rotate(90deg);	-moz-transform: rotate(90deg);		-o-transform: rotate(90deg);	";
       // 注意旋转中点的处理
       style += "-webkit-transform-origin: " + width / 2 + "px " + width / 2 + "px;transform-origin: " + width / 2 + "px " + width / 2 + "px;";   
 		canvas.style.cssText=style;
-		var tmp=width;
-		width=height;
-		height=tmp;
-		alert(width);
-		alert(height);
-
-}
+}else{
+	canvas.width=width;
+	canvas.height=height;
+	canvas.style.cssText="";
+}};
+lookdic();
 ctx.fillStyle="#FFFFFF";
 ctx.fillRect(0,0,width,height);
 var x1,y1,timer,interval=60;
@@ -36,7 +38,7 @@ function clickHandler(e) {
 	x1 = e.pageX;
     y1 = e.pageY;
 	
-	if(screenWidth<screenHeight){
+	if(width<height){
 		var tmp=y1;
 		y1=height-x1;
 		x1=tmp;
@@ -58,10 +60,6 @@ function clickHandler(e) {
 addEventListener("keydown", function (e) {
 	if(e.keyCode===32||e.keyCode===13){parse();}
 }, false);
-function resizewd(){
-	canvas.width=width;
-	canvas.height=height;
-}
 function loop(){
 	ctx.clearRect(0, 0,width,height);
 	perrender();
