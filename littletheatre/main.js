7c5fc9f2dcab0e3afb4133fb773c36afc1c331fb
whiteram <17860971637@139.com>
2018-08-10 14:05:49 +0800

    lt fix 5

--- littletheatre/main.js
+++ littletheatre/main.js
@@ -27,10 +27,8 @@ if( rotate ){
       $print =  $('#print');
       $print.width(height);
       $print.height(width);
-      $print.css('top',  (height-width)/2 );
-      $print.css('left',  0-(height-width)/2 );
-      $print.css('transform' , 'rotate(90deg)');
-      $print.css('transform-origin' , '50% 50%');
+      //$print.css('top',  (height-width)/2 );
+      //$print.css('left',  0-(height-width)/2 );
 		canvas.width=height;
 		canvas.height=width;
 		var tmp=width;
