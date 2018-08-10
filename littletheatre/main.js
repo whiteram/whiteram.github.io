e9573a40fccee7c621601268ff6c3e4e9610087c
whiteram <17860971637@139.com>
2018-08-10 13:01:32 +0800

    lt fix 3

--- littletheatre/main.js
+++ littletheatre/main.js
@@ -1,15 +1,42 @@
 "use strict";
-
+var rotate=false;
+function island(){
+	if (window.orientation === 180 || window.orientation === 0) {
+            alert('竖屏状态！');
+			rotate=true;
+        }
+    if (window.orientation === 90 || window.orientation === -90 ){
+            alert('横屏状态！');
+			rotate=false;
+    } 
+}
+island();
+window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", island, false);
 var canvas=document.getElementById("myCanvas");
 var ctx = canvas.getContext('2d');
 var screenWidth = window.innerWidth;
 var screenHeight = window.innerHeight;
 var clientw = document.documentElement.clientWidth;
 var clienth =document.documentElement.clientHeight;
-var height = screenHeight;
-var width = screenWidth;
+var height = clienth;
+var width = clientw;
 canvas.width=width;
 canvas.height=height;
+if( rotate ){
+      console.log(width + " " + height);
+      $print =  $('#print');
+      $print.width(height);
+      $print.height(width);
+      $print.css('top',  (height-width)/2 );
+      $print.css('left',  0-(height-width)/2 );
+      $print.css('transform' , 'rotate(90deg)');
+      $print.css('transform-origin' , '50% 50%');
+		canvas.width=height;
+		canvas.height=width;
+		var tmp=width;
+		width=height;
+		height=tmp;
+}
 ctx.fillStyle="#FFFFFF";
 ctx.fillRect(0,0,width,height);
 var x1,y1,timer,interval=60;
@@ -23,7 +50,10 @@ function restart(){
 function clickHandler(e) {
 	x1 = e.pageX - canvas.getBoundingClientRect().left;
     y1 = e.pageY - canvas.getBoundingClientRect().top;
-	
+	if(rotate){
+		y1=height-x1;
+		x1=y1;
+	}
 	if (coivisible === true) {
       parsecoi(x1, y1);
     } else if (y1 > txtimgy) {
